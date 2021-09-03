b940619463c08136b9995f06349f8d323bc4294b
Siyabonga Makwelo <Siyabonga.Makwelo@metropolitan.co.za>
2021-09-03 13:38:27 +0200

    sds

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $hubVerifyToken = 'myBusiness23';
 
 
 $challenge = $_GET['hub_challenge'];
-$verify_token = $_Get['hub_verify_token'];
+$verify_token = $_GET['hub_verify_token'];
 
 if($hubVerifyToken === $verify_token){
 echo $challenge;
@@ -12,7 +12,7 @@ exit;
 
 $accessToken = "EAAHhOHJDpuQBAA1QDp81xnY2rZBZBrsN8BDMrZAtkG0ZCZAd1H2exR8tZA6Ec7Vxzo81vURNAynZAxgAf5ZBuiZAj2lcPdsksJJMJZA5DpdocMe2ZAOsW6qU2p3YapLfgSt645LVR0vjJJ8Ghut7y0ZAyDe8ZAbyWKq88uZBdIPk1PJc5qRodJPZAm67UMI4Eh4hoplh9gZD";
 
-$response = json_decode(file_get_contents('php://input'),true);
+$response = file_get_contents('php://input');
 
 file_put_contents("text.txt", $response);
 
